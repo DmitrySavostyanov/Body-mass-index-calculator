9a1bcd45e77fd8f3e7d6ad98aee3e819f5abf21b
Dmitry Savostyanov <dokken671@gmail.com>
2022-09-04 22:05:20 +0700

    first commit - mod --> gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double weight = 55;//Вес в кг (входной параметр: weight типа double)
+        int height = 160;//Рост в см (входной параметр: height типа int)
+        String myBodyMassIndex = String.valueOf(service.calculate(weight, height));
+        System.out.println(myBodyMassIndex);
+    }
 }
